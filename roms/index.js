4496a6b9c0d8f3218d99290d609442ffb2d65576
徐俊杰 <964261897@qq.com>
2023-07-30 16:06:59 +0800

    deploy

---
+++ roms/index.js
@@ -0,0 +1,11 @@
+function getRomsList() {
+  return [
+    {
+      id: '1001',
+      name: '火焰纹章之圣战系谱(简体中文)',
+      file: '1001.zip',
+      platform: 'sfc',
+      core: 'snes9x2010',
+    }
+  ];
+}
